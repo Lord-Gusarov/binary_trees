21eafaca8e7308d2d610ce0334a3cb8ebf6c9cd1
Gustavo Hornedo <2367@holbertonschool.com>
2021-03-09 16:54:02 +0000

    Removing unecessarry prototypes with static qualifier

--- binary_trees.h
+++ binary_trees.h
@@ -25,8 +25,6 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 
 /*---Functions for visualization---*/
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-static size_t _height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *);
 /*---------------------------------*/
 
