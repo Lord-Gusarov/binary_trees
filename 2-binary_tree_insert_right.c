ee00910cd8ecedf205a2765dede71dafe13579b9
Gustavo Hornedo <2367@holbertonschool.com>
2021-03-10 12:26:39 +0000

    Returns Null is arg @parent is NULL

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,7 +2,9 @@
 
 /**
  * binary_tree_insert_right - Inserts a node as the right-child of
- * another node
+ * another node. If parent already has a right-child, the new node will take
+ * its place, and the old right-child will be set as the right-child of the
+ * new node.
  * @parent: pointer to the node to insert the right-child
  * @value: value to store in the new node
  *
@@ -10,24 +12,24 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = malloc(sizeof(binary_tree_t)), *tmp;
+	binary_tree_t *new, *tmp;
 
+	if (!parent)
+		return (NULL);
+
+	new  = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (new);
+
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
-	new->right = NULL;
-	if (parent)
-	{
-		tmp = parent->right;
-		parent->right = new;
-		if (tmp)
-		{
-			tmp->parent = new;
-			new->right = tmp;
-		}
-	}
+
+	tmp = parent->right;
+	parent->right = new;
+	if (tmp)
+		tmp->parent = new;
+	new->right = tmp; /*Works even if tmp points to NULL*/
 
 	return (new);
 }
